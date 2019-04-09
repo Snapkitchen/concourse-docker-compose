2b62328d8519da550dc777deff7cd7cbda182768
Ethan Edwards <ethan_edwards@snapkitchen.com>
2019-04-09 12:21:44 -0500

    use dind image and use busybox version of timeout args

--- lib/common.bash
+++ lib/common.bash
@@ -66,17 +66,6 @@ start_docker() {
 
   local mtu=$(cat /sys/class/net/$(ip route get 8.8.8.8|awk '{ print $5 }')/mtu)
   local server_args="--mtu ${mtu}"
-  local registry=""
-
-  server_args="${server_args} --max-concurrent-downloads=$1 --max-concurrent-uploads=$2"
-
-  for registry in $3; do
-    server_args="${server_args} --insecure-registry ${registry}"
-  done
-
-  if [ -n "$4" ]; then
-    server_args="${server_args} --registry-mirror $4"
-  fi
 
   try_start() {
     dockerd --data-root /scratch/docker ${server_args} >$LOG_FILE 2>&1 &
@@ -97,7 +86,7 @@ start_docker() {
   declare -fx try_start
   trap stop_docker EXIT
 
-  if ! timeout ${STARTUP_TIMEOUT} bash -ce 'while true; do try_start && break; done'; then
+  if ! timeout -t ${STARTUP_TIMEOUT} bash -ce 'while true; do try_start && break; done'; then
     echo Docker failed to start within ${STARTUP_TIMEOUT} seconds.
     return 1
   fi
